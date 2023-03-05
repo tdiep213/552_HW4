5636d63604f15f474a5a50b1cf23ef7eb12f7af1
Harry Kiffel <kiffel@vm-instunix-02.cs.wisc.edu>
2023-03-04 20:53:06 -0600

    barebones nested if test

--- hw4/hw4_2/p2-2.asm
+++ hw4/hw4_2/p2-2.asm
@@ -3,3 +3,48 @@
 // Why is Branch prediction important and necessary
 // Performs a bunch of nested if statements.
 // While not an optimal implementation, we can't assume that it will always be optimal at every stage.
+
+// The deepest nested path will result in r4 = 4, and each missed if-branch will lower the final value by 1.
+
+// Can add stress-test operations within each THEN label
+
+LBI r4, 0
+// If 1
+BEQZ Rs, .THEN1
+.ELSE1:
+ADDI r4, r4, -1
+
+.THEN1:
+ADDI r4, r4,  1
+// If 2
+BEQZ Rs, .THEN2
+
+.ELSE2:
+ADDI r4, r4, -1
+
+.THEN2:
+ADDI r4, r4,  1
+// If 3
+BEQZ Rs, .THEN3
+
+.ELSE3:
+ADDI r4, r4, -1
+
+.THEN3:
+ADDI r4, r4,  1
+// If 4
+BEQZ Rs, .THEN4  
+
+.ELSE4:
+ADDI r4, r4, -1
+
+.THEN4:
+ADDI r4, r4,  1
+BEQZ Rs, .THEN5  
+
+.ELSE5:
+ADDI r4, r4,  -1
+
+.THEN5:
+
+
